91511bc8e10c967d2b16f64381af4be23d599bb4
Fran72 <francisco_llorente@hotmail.com>
2024-12-03 23:35:06 +0100

    countdown funcionando ... falta maquetar

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,22 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Countdown from 'react-countdown';
+
+const renderer = ({ minutes, seconds, completed }: { minutes: number, seconds: number, completed: boolean }) => {
+  let formattedMinutes = String(minutes);
+  let formattedSeconds = String(seconds);
+  if (minutes < 10) { formattedMinutes = '0' + String(minutes); }
+  if (seconds < 10) { formattedSeconds = '0' + String(seconds); }
+  
+  if (completed) {
+    // Render a completed state
+    return <span> Time is out...click to reload </span>;
+  } else {
+    // Render a countdown
+    return <span className='counter'>{formattedMinutes}:{formattedSeconds}</span>;
+  }
+};
 
 function App() {
   const [count, setCount] = useState(0)
@@ -19,6 +35,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
+      <Countdown date={Date.now() + 66000} renderer={renderer} />
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={fun}>
